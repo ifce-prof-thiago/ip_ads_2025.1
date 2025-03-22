944f125b6974d3a148df7da69568ff4a6b0621bf
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-22 15:10:20 -0300

    Resolução ex05 e ex06

--- exercicios/05.cpp
+++ exercicios/05.cpp
@@ -2,3 +2,15 @@
 Escreva um programa que calcule o IMC de um indivíduo, utilizando a seguinte fórmula
 IMC = massa / altura²
 */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    float massa;
+    float altura;
+    cin >> massa;
+    cin >> altura;
+    cout << "IMC: " << massa / pow(altura, 2) << endl;
+}
--- exercicios/06.cpp
+++ exercicios/06.cpp
@@ -1,3 +1,14 @@
 /*
 Crie um programa que calcule e exiba o perímetro de uma circunferência, solicitando o raio ao usuário.
+C=2*PI*r
 */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  float raio;
+  cin >> raio;
+  cout << "Perímetro: " << 2 * M_PI * raio << endl;
+}
