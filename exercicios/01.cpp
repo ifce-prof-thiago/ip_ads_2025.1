5b3b9256fa9b49cbc532df6c63fcc29c46635cec
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-22 14:23:50 -0300

    Resolução do ex01

--- exercicios/01.cpp
+++ exercicios/01.cpp
@@ -2,3 +2,18 @@
 Escreva um programa que solicite ao usuário dois números e exiba a soma, subtração, multiplicação e a divisão
 entre eles
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float numero1;
+    float numero2;
+    cin >> numero1;
+    cin >> numero2;
+    cout << "Soma: " << numero1 + numero2 << endl;
+    cout << "Subtração: " << numero1 - numero2 << endl;
+    cout << "Multiplicação: " << numero1 * numero2 << endl;
+    cout << "Divisão: " << numero1 / numero2 << endl;
+}
