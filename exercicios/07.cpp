4df82e5b88341b67ce87c3977fb7c69639c61f6f
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-22 15:17:20 -0300

    Ex01

--- exercicios/07.cpp
+++ exercicios/07.cpp
@@ -2,3 +2,13 @@
 Escreva um programa que calcule a área de uma circunferência a partir do raio, utilizando a fórmula
 A = PI * r²
 */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    float raio;
+    cin >> raio;
+    cout << "A: " << M_PI * pow(raio, 2) << endl;
+}
