a27ed68d1587fb08649819c055979cce852a27b0
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-22 14:33:54 -0300

    Resolução do ex03

--- exercicios/03.cpp
+++ exercicios/03.cpp
@@ -1,3 +1,16 @@
 /*
 Crie um programa que calcule e exiba a média aritimética de três notas informadas pelo usuário.
 */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float n1;
+    float n2;
+    float n3;
+    cin >> n1;
+    cin >> n2;
+    cin >> n3;
+    cout << "Média aritmética: " << (n1 + n2 + n3) / 3 << endl;
+}
