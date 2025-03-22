b067331d0d1207e6ad87d81812f794a2a3b6a481
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-22 14:29:18 -0300

    Resolução do ex02

--- exercicios/02.cpp
+++ exercicios/02.cpp
@@ -1,3 +1,17 @@
 /*
 Escreva um programa que calcule a média aritimética de dois números.
 */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  float n1;
+  float n2;
+
+  cin >> n1;
+  cin >> n2;
+
+  cout << "Média aritmética: " << (n1 + n2) / 2 << endl;
+
+}
