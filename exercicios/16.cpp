7e9a999591708d8017b6b2c15f84836e62154a62
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-29 14:34:58 -0300

    ex16

--- exercicios/16.cpp
+++ exercicios/16.cpp
@@ -1,3 +1,14 @@
 /*
 Crie um programa que leia duas palavras e as contcatene, exibindo a palavra resultante.
 */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    string palavra1;
+    string palavra2;
+    cin >> palavra1 >> palavra2;
+    printf("%s%s\n", palavra1.c_str(), palavra2.c_str());
+}
