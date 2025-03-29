0b02e641bc072f60500bbf1b3dfe3c20b056be77
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-29 15:21:35 -0300

    ex17

--- exercicios/17.cpp
+++ exercicios/17.cpp
@@ -2,3 +2,14 @@
 Faça um programa que receba uma palavra e exiba cada letra separadamente.
 Atenção: Necessita de um laço de repetição.
 */
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    string palavra;
+    cin >> palavra;
+    for (int i = 0; i < palavra.size(); i = i + 1) {
+        printf("%c\n", palavra[i]);
+    }
+}
