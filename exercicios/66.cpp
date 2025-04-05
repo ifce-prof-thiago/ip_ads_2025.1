884063554c35535e4adcc3bbb75ff045f90e3f12
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-04-05 15:13:57 -0300

    Ex66

--- exercicios/66.cpp
+++ exercicios/66.cpp
@@ -1,3 +1,22 @@
 /*
 Escreva um programa que leia um vetor de números inteiros e exiba o maior elemento presente no vetor
  */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int numbers[n];
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+    int maior = numbers[0];
+    for (int i = 0; i < n; i++) {
+        if (numbers[i] > maior) {
+            maior = numbers[i];
+        }
+    }
+    printf("%d\n", maior);
+}
