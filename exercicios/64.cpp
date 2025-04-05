7ba9bc86b9c348d7a80eb2f14107be053f7200e8
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-04-05 14:57:48 -0300

    Ex64

--- exercicios/64.cpp
+++ exercicios/64.cpp
@@ -2,3 +2,20 @@
 Elabore um programa que primeiro receba um valor N e, em seguida, leia N números.
 Ao final, apresente a soma de todos esses valores.
  */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int numeros[n];
+    for (int i = 0; i < n; i++) {
+        cin >> numeros[i];
+    }
+    int soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma = soma + numeros[i];
+    }
+    printf("%d\n", soma);
+}
